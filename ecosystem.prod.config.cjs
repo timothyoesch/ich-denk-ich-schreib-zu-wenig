7681d208801798c633699eefeda57e963f1dedf2
Timothy Justin Oesch <timothy@kpunkt.ch>
2026-04-14 14:12:33 +0200

    Add dotenv and read file manually

--- ecosystem.prod.config.cjs
+++ ecosystem.prod.config.cjs
@@ -1,11 +1,17 @@
+const dotenv = require("dotenv");
+const envConfig = dotenv.config({ path: ".env.production" }).parsed || {};
 module.exports = {
-    apps: [
-        {
-            name: 'toes.ch Blog',
-            port: '8702',
-            exec_mode: 'cluster',
-            instances: 'max',
-            script: './.output/server/index.mjs',
-        }
-    ]
-}
+  apps: [
+    {
+      name: "toes.ch Blog",
+      port: "8702",
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./.output/server/index.mjs",
+      env: {
+        NODE_ENV: "production",
+        ...envConfig,
+      },
+    },
+  ],
+};
