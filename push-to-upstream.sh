6d9593120710212eb63fcee93a24529ea712e762
Timothy Justin Oesch <timothy@kpunkt.ch>
2026-04-15 23:20:11 +0200

    Sync downstream 15/04/2026 23:20:11

--- push-to-upstream.sh
+++ push-to-upstream.sh
@@ -5,9 +5,10 @@ find content/posts -type f -name "*.md" | while read -r file; do
         echo "===> $file"
         echo "-----------------------------------"
         exit 1
+    else
+        git pull upstream main
+        git add -A
+        git commit -m "Sync downstream $(date '+%d/%m/%Y %H:%M:%S')"
+        git push upstream main
     fi
 done
-git pull upstream main
-git add -A
-git commit -m "Sync downstream $(date '+%d/%m/%Y %H:%M:%S')"
-git push upstream main
